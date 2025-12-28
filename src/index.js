4ba788155733e6be011435b5b06796aeaaa9d536
kami <darksideinfinity983@gmail.com>
2025-12-28 14:03:58 +0100

    mise a jour des des différentes fontion utilitaire du doosier lib et initalisation du code du pint de depart dans index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import dotenv from "dotenv"
 
+dotenv.config()
+
+import { logger, httpLogger } from "#/lib/logger"
+import {errorHandler } from "#middlawares/error-handler"
+import {notFoundHandler} from "#middlewares/not-found"
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+//Middlewares
+app.use(helmet())
+app.use(cors())
+app.use(httpLogger)
+app.use(express.json())
+
+//routes
+app.get("/", (res, req)=>{
+ res.json({
+    success: true,
+    message: "PAI Express operationnelle"
+ })
+})
+
+app.use(notFoundHandler)
+// Global error handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Serveur démarré sur <http://localhost>:${PORT}`);
+});
