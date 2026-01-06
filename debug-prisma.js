65377f61b9e6c5e1ac33fe4eae2db2a33158b51f
Jean-Marie <oktcjean@gmail.com>
2026-01-06 08:35:27 +0100

    Revert "Dev 2 - Authentification complète fonctionnelle"
    This reverts commit 36d3c88e8fb908001d819e32e26c8f18f9b3f57a.

--- debug-prisma.js
+++
@@ -1,5 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const adapterModule = require('@prisma/adapter-better-sqlite3');
-console.log('Adapter Module:', adapterModule);
-console.log('Keys:', Object.keys(adapterModule));
--- src/lib/prisma.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const { PrismaClient } = require('@prisma/client');
-
-export const prisma = new PrismaClient();
